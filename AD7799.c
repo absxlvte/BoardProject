b42efff4be8deba267d9385952cec622acba2648
absxlvte <zotin1000@gmail.com>
2025-09-22 15:07:11 +0300

    checked: MOSI clear, MISO idk

--- AD7799.c
+++ AD7799.c
@@ -1,6 +1,5 @@
 #include "AD7799.h"
 SPI_HandleTypeDef xSPI;
-uint8_t xID = 0;
 void SPI_Init(void){
 	GPIO_InitTypeDef SPI_GPIO;
 	__HAL_RCC_SPI2_CLK_ENABLE();
@@ -12,67 +11,73 @@ void SPI_Init(void){
 	SPI_GPIO.Alternate=GPIO_AF5_SPI2;
 	HAL_GPIO_Init(GPIOB,&SPI_GPIO);
 	
+	GPIO_InitTypeDef CS_GPIO;
+	CS_GPIO.Pin=AD_CS;
+	CS_GPIO.Mode=GPIO_MODE_OUTPUT_PP;
+	CS_GPIO.Speed=GPIO_SPEED_FREQ_HIGH;
+	HAL_GPIO_Init(GPIOB,&CS_GPIO);
+	
+	
 	xSPI.Instance=SPI2;
 	xSPI.Init.Mode=SPI_MODE_MASTER;
 	xSPI.Init.Direction=SPI_DIRECTION_2LINES ;
 	xSPI.Init.DataSize=SPI_DATASIZE_8BIT;
-	xSPI.Init.CLKPolarity=SPI_POLARITY_LOW;	//CPOL=1
-	xSPI.Init.CLKPhase=SPI_PHASE_2EDGE;  		//CPHA=1
+	xSPI.Init.CLKPolarity=SPI_POLARITY_HIGH;	//
+	xSPI.Init.CLKPhase=SPI_PHASE_2EDGE;  		//
 	//xSPI.Init.NSS=SPI_NSS_SOFT;     			// SOFT or HARD?
 	xSPI.Init.NSS=SPI_NSS_HARD_OUTPUT;      // SOFT or HARD?
-	xSPI.Init.BaudRatePrescaler=SPI_BAUDRATEPRESCALER_2; //32 or 64 or ... ?
+	xSPI.Init.BaudRatePrescaler=SPI_BAUDRATEPRESCALER_128; //32 or 64 or ... ?
 	xSPI.Init.FirstBit=SPI_FIRSTBIT_MSB;
 	HAL_SPI_Init(&xSPI);
-	//HAL_NVIC_EnableIRQ(SPI2_IRQn); 
 }
 
 
-uint8_t AD7799_Init(void) {
-    //HAL_Delay(10);
-    //uint8_t reset_cmd[4] = {0xFF, 0xFF, 0xFF, 0xFF};
-    //HAL_SPI_Transmit(&xSPI, reset_cmd, 4, 100);
-    //HAL_Delay(1);
-    uint8_t id = AD7799_ReadRegister(AD7799_REG_ID, 1);
-		//uint8_t id = AD7799_ReadRegister(AD7799_REG_STATUS, 1);
-    return id;
+void AD7799_SetRegisterValue(unsigned char regAddress,
+														 unsigned long regValue, 
+														 unsigned char size) {
+	unsigned char data[4] = {0x00, 0x00, 0x00, 0x00};	
+	data[0] = AD7799_COMM_WRITE |  AD7799_COMM_ADDR(regAddress);
+	switch (size){
+		case 1:
+      data[1] = (unsigned char)regValue;
+			break;
+    case 2:
+			data[2] = (unsigned char)((regValue & 0x0000FF) >> 0);
+      data[1] = (unsigned char)((regValue & 0x00FF00) >> 8);
+			break;
+    case 3:
+			data[3] = (unsigned char)((regValue & 0x0000FF) >> 0);
+			data[2] = (unsigned char)((regValue & 0x00FF00) >> 8);
+      data[1] = (unsigned char)((regValue & 0xFF0000) >> 16);
+			break;
+	}
+	AD7799_CS_LOW;	    
+	HAL_SPI_Transmit(&xSPI, data, 1+size, 100);
+	AD7799_CS_HIGH;
 }
-
-HAL_StatusTypeDef AD7799_WriteRegister(uint8_t reg, uint16_t value) {
-    uint8_t tx_data[3];
-    tx_data[0] = (0 << 7) | (1 << 6) | ((reg & 0x7) << 3);
-    tx_data[1] = value >> 8;
-    tx_data[2] = value & 0xFF;
-    
-    return HAL_SPI_Transmit(&xSPI, tx_data, 3, 100);
-}
-
-uint32_t AD7799_ReadRegister(uint8_t reg, uint8_t size) {
-    uint8_t tx_data[4] = {0};
-    uint8_t rx_data[4] = {0};
-    
-    tx_data[0] = (0 << 7) | (1 << 6) | ((reg & 0x7) << 3);
-    
-    HAL_SPI_TransmitReceive(&xSPI, tx_data, rx_data, size+1, 100);
-    
-//		HAL_SPI_Transmit(&xSPI, tx_data, size, 100);
-//		HAL_Delay(10);
-//		HAL_SPI_Transmit(&xSPI, rx_data, size, 100);
-		
-    uint32_t result = 0;
-    for(int i = 0; i < size; i++) {
-        result = (result << 8) | rx_data[i + 1];
-    }
-    
-    return result;
-}
-
-HAL_StatusTypeDef AD7799_SetChannel(uint8_t channel, uint8_t gain) {
-    uint16_t config = (1 << 4) | (gain << 8) | (channel & 0x7);
-    AD7799_WriteRegister(AD7799_REG_CONFIG, config);
-    uint16_t mode = (AD7799_MODE_CONT << 13) | (AD7799_RATE_16_7HZ << 0);
-    return AD7799_WriteRegister(AD7799_REG_MODE, mode);
-}
-
-uint32_t AD7799_ReadData(void) {
-    return AD7799_ReadRegister(AD7799_REG_DATA, 3);
+unsigned long AD7799_GetRegisterValue(unsigned char regAddress, 
+																			unsigned char size){
+	unsigned char data[4] = {0x00, 0x00, 0x00, 0x00};
+	unsigned long receivedData = 0x00;	
+	data[0] = AD7799_COMM_READ | AD7799_COMM_ADDR(regAddress);
+	AD7799_CS_LOW;  
+	HAL_SPI_Transmit(&xSPI, data, 1, 100);
+	HAL_Delay(10);
+	HAL_SPI_Receive(&xSPI, data, size, 100);
+	AD7799_CS_HIGH;
+	switch (size){
+		case 1:
+			receivedData += (data[0] << 0);
+			break;
+		case 2:
+			receivedData += (data[0] << 8);
+			receivedData += (data[1] << 0);
+			break;
+		case 3:
+			receivedData += (data[0] << 16);
+			receivedData += (data[1] << 8);
+			receivedData += (data[2] << 0);
+			break;
+	}
+    return receivedData;
 }
--- AD7799.h
+++ AD7799.h
@@ -3,66 +3,101 @@
 #include "stm32f4xx.h"                 
 #include "stm32f4xx_hal.h"
 
+#define AD7799_CS_LOW 	HAL_GPIO_WritePin(GPIOB,AD_CS,GPIO_PIN_RESET)
+#define AD7799_CS_HIGH  HAL_GPIO_WritePin(GPIOB,AD_CS,GPIO_PIN_SET)
+
 #define AD_SCLK GPIO_PIN_13 //PB13
 #define AD_DIN  GPIO_PIN_15 //PB15
 #define AD_DOUT GPIO_PIN_14 //PB14
-//registers
-#define AD7799_REG_COMM        0x00
-#define AD7799_REG_STATUS      0x00
-#define AD7799_REG_MODE        0x01
-#define AD7799_REG_CONFIG      0x02
-#define AD7799_REG_DATA        0x03
-#define AD7799_REG_ID          0x04
-#define AD7799_REG_IO          0x05
-#define AD7799_REG_OFFSET      0x06
-#define AD7799_REG_FULLSCALE   0x07
-//channels
-#define AD7799_CH_AIN1         0x00
-#define AD7799_CH_AIN2         0x01
-#define AD7799_CH_AIN3         0x02
-//gain
-#define AD7799_GAIN_1          0x00
-#define AD7799_GAIN_2          0x01
-#define AD7799_GAIN_4          0x02
-#define AD7799_GAIN_8          0x03
-#define AD7799_GAIN_16         0x04
-#define AD7799_GAIN_32         0x05
-#define AD7799_GAIN_64         0x06
-#define AD7799_GAIN_128        0x07
-//mode
-#define AD7799_MODE_CONT       0x00
-#define AD7799_MODE_SINGLE     0x01
-#define AD7799_MODE_IDLE       0x02
-#define AD7799_MODE_PWRDN      0x03
-//freqs
-#define AD7799_RATE_470HZ      0x00
-#define AD7799_RATE_242HZ      0x01
-#define AD7799_RATE_123HZ      0x02
-#define AD7799_RATE_62HZ       0x03
-#define AD7799_RATE_50HZ       0x04
-#define AD7799_RATE_39HZ       0x05
-#define AD7799_RATE_33_2HZ     0x06
-#define AD7799_RATE_19_6HZ     0x07
-#define AD7799_RATE_16_7HZ     0x08
-#define AD7799_RATE_16_7HZ_2   0x09
-#define AD7799_RATE_12_5HZ     0x0A
-#define AD7799_RATE_10HZ       0x0B
-#define AD7799_RATE_8_33HZ     0x0C
-#define AD7799_RATE_6_25HZ     0x0D
-#define AD7799_RATE_4_17HZ     0x0E
+#define AD_CS 	GPIO_PIN_11 //PB11
 
-uint8_t AD7799_Init(void);
-HAL_StatusTypeDef AD7799_WriteRegister(uint8_t reg, uint16_t value);
-uint32_t AD7799_ReadRegister(uint8_t reg, uint8_t size);
-HAL_StatusTypeDef AD7799_SetChannel(uint8_t channel, uint8_t gain);
-uint32_t AD7799_ReadData(void);
+#define AD7799_REG_COMM		0 /* Communications Register(WO, 8-bit) */
+#define AD7799_REG_STAT	    0 /* Status Register	    (RO, 8-bit) */
+#define AD7799_REG_MODE	    1 /* Mode Register	     	(RW, 16-bit */
+#define AD7799_REG_CONF	    2 /* Configuration Register (RW, 16-bit)*/
+#define AD7799_REG_DATA	    3 /* Data Register	     	(RO, 16-/24-bit) */
+#define AD7799_REG_ID	    4 /* ID Register	     	(RO, 8-bit) */
+#define AD7799_REG_IO	    5 /* IO Register	     	(RO, 8-bit) */
+#define AD7799_REG_OFFSET   6 /* Offset Register	    (RW, 24-bit */
+#define AD7799_REG_FULLSALE	7 /* Full-Scale Register	(RW, 24-bit */
 
-  
+/* Communications Register Bit Designations (AD7799_REG_COMM) */
+#define AD7799_COMM_WEN		(1 << 7) 			/* Write Enable */
+#define AD7799_COMM_WRITE	(0 << 6) 			/* Write Operation */
+#define AD7799_COMM_READ    (1 << 6) 			/* Read Operation */
+#define AD7799_COMM_ADDR(x)	(((x) & 0x7) << 3)	/* Register Address */
+#define AD7799_COMM_CREAD	(1 << 2) 			/* Continuous Read of Data Register */
+
+/* Status Register Bit Designations (AD7799_REG_STAT) */
+#define AD7799_STAT_RDY		(1 << 7) /* Ready */
+#define AD7799_STAT_ERR		(1 << 6) /* Error (Overrange, Underrange) */
+#define AD7799_STAT_CH3		(1 << 2) /* Channel 3 */
+#define AD7799_STAT_CH2		(1 << 1) /* Channel 2 */
+#define AD7799_STAT_CH1		(1 << 0) /* Channel 1 */
+
+/* Mode Register Bit Designations (AD7799_REG_MODE) */
+#define AD7799_MODE_SEL(x)		(((x) & 0x7) << 13)	/* Operation Mode Select */
+#define AD7799_MODE_PSW(x)		(1 << 12)			/* Power Switch Control Bit */	
+#define AD7799_MODE_RATE(x)		((x) & 0xF) 		/* Filter Update Rate Select */
+
+/* AD7799_MODE_SEL(x) options */
+#define AD7799_MODE_CONT		 0 /* Continuous Conversion Mode */
+#define AD7799_MODE_SINGLE		 1 /* Single Conversion Mode */
+#define AD7799_MODE_IDLE		 2 /* Idle Mode */
+#define AD7799_MODE_PWRDN		 3 /* Power-Down Mode */
+#define AD7799_MODE_CAL_INT_ZERO 4 /* Internal Zero-Scale Calibration */
+#define AD7799_MODE_CAL_INT_FULL 5 /* Internal Full-Scale Calibration */
+#define AD7799_MODE_CAL_SYS_ZERO 6 /* System Zero-Scale Calibration */
+#define AD7799_MODE_CAL_SYS_FULL 7 /* System Full-Scale Calibration */
+
+/* Configuration Register Bit Designations (AD7799_REG_CONF) */
+#define AD7799_CONF_BO_EN	  (1 << 13) 			/* Burnout Current Enable */
+#define AD7799_CONF_UNIPOLAR  (1 << 12) 			/* Unipolar/Bipolar Enable */
+#define AD7799_CONF_GAIN(x)	  (((x) & 0x7) << 8) 	/* Gain Select */
+#define AD7799_CONF_REFDET(x) (((x) & 0x1) << 5) 	/* Reference detect function */
+#define AD7799_CONF_BUF		  (1 << 4) 				/* Buffered Mode Enable */
+#define AD7799_CONF_CHAN(x)	  ((x) & 0x7) 			/* Channel select */
+
+/* AD7799_CONF_GAIN(x) options */
+#define AD7799_GAIN_1       0
+#define AD7799_GAIN_2       1
+#define AD7799_GAIN_4       2
+#define AD7799_GAIN_8       3
+#define AD7799_GAIN_16      4
+#define AD7799_GAIN_32      5
+#define AD7799_GAIN_64      6
+#define AD7799_GAIN_128     7
+
+/* AD7799_CONF_REFDET(x) options */
+#define AD7799_REFDET_ENA   1	
+#define AD7799_REFDET_DIS   0
+
+/* AD7799_CONF_CHAN(x) options */
+#define AD7799_CH_AIN1P_AIN1M	0 /* AIN1(+) - AIN1(-) */
+#define AD7799_CH_AIN2P_AIN2M	1 /* AIN2(+) - AIN2(-) */
+#define AD7799_CH_AIN3P_AIN3M	2 /* AIN3(+) - AIN3(-) */
+#define AD7799_CH_AIN1M_AIN1M	3 /* AIN1(-) - AIN1(-) */
+#define AD7799_CH_AVDD_MONITOR	7 /* AVDD Monitor */
+
+/* ID Register Bit Designations (AD7799_REG_ID) */
+#define AD7799_ID			0x9
+#define AD7799_ID_MASK		0xF
+
+/* IO (Excitation Current Sources) Register Bit Designations (AD7799_REG_IO) */
+#define AD7799_IOEN			(1 << 6)
+#define AD7799_IO1(x)		(((x) & 0x1) << 4)
+#define AD7799_IO2(x)		(((x) & 0x1) << 5)
+
 extern SPI_HandleTypeDef xSPI;
-extern uint8_t xID;
 
 void SPI_Init(void);
-//void SPI2_IRQHandler(void);
+void AD7799_SetRegisterValue(unsigned char regAddress,unsigned long regValue,unsigned char size);
+unsigned long AD7799_GetRegisterValue(unsigned char regAddress, unsigned char size);
+
+
+  
+
+
 
 
 #endif
