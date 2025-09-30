e27f7f60d1d08d5416bf18f456316140d9cb5d3b
absxlvte <zotin1000@gmail.com>
2025-09-30 13:15:22 +0300

    cplt LMT70; next TMP119

--- pyUSB/main.py
+++ pyUSB/main.py
@@ -1,6 +1,7 @@
 import serial
 import os
-
+import time
+import math
 
 def AD7799_ConvTo_mV(Data, Vref, Gain, N, Pol):
     if Pol:
@@ -12,12 +13,22 @@ def AD7799_ConvTo_mV(Data, Vref, Gain, N, Pol):
 
 
 ser = serial.Serial('COM5', 115200, timeout=1)
-
+k = 0
+win = 5
+buf = []
 while True:
     data = ser.read(3)
     if len(data) == 3:
-        os.system('cls')
+
         int_le = int.from_bytes(data, 'little')
         mv = AD7799_ConvTo_mV(int_le,2.5,2.0,24,0)
         Temperature = -8.451576E-06 * mv**2 - 1.769281E-01 * mv + 2.043937E+02;
-        print(f"ADC: {data}, ADC int: {int_le},ADC mv: {mv}, Temperature: {Temperature}")
+        buf.append(round(Temperature, 2))
+        k += 1
+        if k>=win:
+            os.system('cls')
+            result = sorted(buf)[math.ceil(win/2)]
+            buf = []
+            k = 0
+            print(f"Temperature: {result}\n")
+        #print(f"ADC: {data}, ADC int: {int_le},ADC mv: {mv}, Temperature: {result}")
