d8b1d312923289e642053a19177dd99b53e5b69d
absxlvte <zotin1000@gmail.com>
2025-10-29 15:51:34 +0300

    rdy for expimerents

--- pyUSB/main.py
+++ pyUSB/main.py
@@ -1,7 +1,7 @@
 import serial
 import os
+import csv
 import time
-import math
 
 def AD7799_ConvTo_mV(Data, Vref, Gain, N, Pol):
     if Pol:
@@ -12,31 +12,32 @@ def AD7799_ConvTo_mV(Data, Vref, Gain, N, Pol):
 
 try:
     ser = serial.Serial('COM5', 115200, timeout=0)
-    k = 0
-    win = 5
-    buf = []
-    #filt = True
-    filt = False
+    results = []
+    start_time = time.time()
+    os.makedirs("pyUSB", exist_ok=True)
+    measurement_number = 1
     while True:
         data = ser.read(3)
         if len(data) == 3:
             int_le = int.from_bytes(data, 'little')
-            mv = AD7799_ConvTo_mV(int_le,2.5,2.0,24,0)
-            Temperature = -8.451576E-06 * mv**2 - 1.769281E-01 * mv + 2.043937E+02;
-            if filt:
-                buf.append(round(Temperature, 4))
-                k += 1
-                if k>=win:
-                    os.system('cls')
-                    result = sorted(buf)[math.ceil(win/2)]
-                    buf = []
-                    k = 0
-                    print(f"Temperature (wind): {result}\n")
-            else:
-                os.system('cls')
-                print(f"Temperature (no wind): {Temperature:.4f}\n")
-            #print(f"ADC: {data}, ADC int: {int_le},ADC mv: {mv}, Temperature: {result}")
+            mv = AD7799_ConvTo_mV(int_le, 2.5, 2.0, 24, 0)
+            Temperature = -8.451576E-06 * mv**2 - 1.769281E-01 * mv + 2.043937E+02
+            current_time = time.time() - start_time
+            os.system('cls')
+            print(f"Измерение #{measurement_number}")
+            print(f"Время: {current_time:.3f} c")
+            print(f"Z: {int_le}")
+            print(f"Temperature: {Temperature:.4f} °C\n")
+            results.append([measurement_number, f"{current_time:.3f}", int_le, f"{Temperature:.4f}"])
+            measurement_number += 1
 except KeyboardInterrupt:
-    pass
+    csv_path = "pyUSB/data.csv"
+    with open(csv_path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["№ измерения", "Время (с)", "Z", "Temperature (°C)"])
+        writer.writerows(results)
+    print(f"\nДанные сохранены в {csv_path}")
+
 finally:
-    ser.close()
+    if 'ser' in locals() and ser.is_open:
+        ser.close()
